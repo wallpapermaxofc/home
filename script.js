c2e1f7615c6102e12fecc5d15145eed432bf66d6
Wallpaper Max <wallpapermaxofc@gmail.com>
2024-11-27 22:55:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,19 +7,42 @@ function downloadImage(imageUrl) {
     document.body.removeChild(link);
 }
 
-// Filtragem da galeria com base no termo de pesquisa
+// Filtro na barra de pesquisa
 document.getElementById('searchInput').addEventListener('keyup', function() {
     const searchTerm = this.value.toLowerCase();
     const wallpapers = document.querySelectorAll('.wallpaper');
 
     wallpapers.forEach(wallpaper => {
-        const category = wallpaper.getAttribute('data-category').toLowerCase();
-        const name = wallpaper.getAttribute('data-name').toLowerCase();
-
-        if (category.includes(searchTerm) || name.includes(searchTerm)) {
+        const imgAlt = wallpaper.querySelector('img').alt.toLowerCase();
+        if (imgAlt.includes(searchTerm)) {
             wallpaper.style.display = 'block';
         } else {
             wallpaper.style.display = 'none';
         }
+    });
+});
+
+// Aguarda o carregamento de imagens
+window.addEventListener('load', () => {
+    const gallery = document.getElementById('gallery');
+    const images = gallery.querySelectorAll('img');
+
+    images.forEach(img => {
+        img.onload = () => {
+            img.parentElement.style.visibility = 'visible';
+        };
+    });
+});
+
+window.addEventListener('load', () => {
+    const gallery = document.querySelector('.gallery');
+    const wallpapers = document.querySelectorAll('.wallpaper');
+
+    wallpapers.forEach(item => {
+        const img = item.querySelector('img');
+        img.onload = () => {
+            const height = img.getBoundingClientRect().height;
+            item.style.flexBasis = `${height}px`;
+        };
     });
 });
