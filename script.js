2bd5dbb926a3b31c1428b1e7df54a84fe17c30f6
Wallpaper Max <wallpapermaxofc@gmail.com>
2024-11-28 21:11:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -21,19 +21,6 @@ function filterCategory(category) {
     });
 }
 
-let debounceTimeout;
-document.getElementById('searchInput').addEventListener('keyup', function () {
-    clearTimeout(debounceTimeout);
-    debounceTimeout = setTimeout(() => {
-        const searchTerm = this.value.toLowerCase();
-        const wallpapers = document.querySelectorAll('.wallpaper');
-        wallpapers.forEach(wallpaper => {
-            const altText = wallpaper.querySelector('img').alt.toLowerCase();
-            wallpaper.style.display = altText.includes(searchTerm) ? 'block' : 'none';
-        });
-    }, 300);
-});
-
 function isMobile() {
     return /Mobi|Android/i.test(navigator.userAgent);
 }
